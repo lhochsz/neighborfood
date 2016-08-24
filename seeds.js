99d37fd439d8b9d87457586240f3d1e277e89006
Leslie Hochsztein <lhochsz@gmail.com>
2016-08-24 10:09:00 -0400

    Added database info to seeds.js

--- seeds.js
+++ seeds.js
@@ -3,7 +3,21 @@ var User = require('./models/user');
 var FridgeItem = require('./models/fridge');
 mongoose.Promise = require('bluebird');
 
-mongoose.connect('mongodb://localhost/fridge');
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/fridge');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // our script will not exit until we have disconnected from the db.
 function quit() {
