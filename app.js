cad8dabc8c5784d57ab277b4e2ea1643434de511
Leslie Hochsztein <lhochsz@gmail.com>
2016-08-24 09:57:24 -0400

    Added info to app.js for Heroku database connection

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var methodOverride = require('method-override');
 var passport = require('passport');
 var session = require('express-session');
 var flash = require('connect-flash');
-var multer       = require('multer');
 mongoose.Promise = require('bluebird')
 
 var routes = require('./routes/index');
@@ -19,7 +18,22 @@ var fridge = require('./routes/fridge');
 var app = express();
 
 // Connect to database
-mongoose.connect('mongodb://localhost/fridge');
+
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/fridge');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
